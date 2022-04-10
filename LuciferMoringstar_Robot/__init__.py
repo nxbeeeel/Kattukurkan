b5b00bc71d46c3f71c362dd88511159dd1f07bc9
nxbeeeel <102342835+nxbeeeel@users.noreply.github.com>
2022-04-10 08:39:10 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -28,11 +28,8 @@ Bot Owner Only
 """
 
 ABOUT = """
-➥ Name :  Auto Filter V3
-    
-➥ Creator : [Naughty PROFESSOR](
-https://youtube.com/c/NaughtyPROFESSOR)
-
+➥ Name :  Kattukurkan auto filter bot v3
+   
 ➥ Language : Python3
 
 ➥ Library : Pyrogram Asyncio 1.13.0
